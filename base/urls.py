f6ca7f287728f55b6fc532935e35cb490403c8bc
xandee <davidonzan@gmail.com>
2023-02-17 06:48:17 +0100

    Setting up views

---
+++ base/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.products, name="products"),
+    path("text-generator/", views.textgenView, name="textgen"),
+    path("code-generator/", views.codegenView, name="codegen"),
+    path("image-generator/", views.imagegenView, name="imagegen"),
+]
